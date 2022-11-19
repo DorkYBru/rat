51054eefbad0df4e3ab0c80b41d4b83e46f98207
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2022-11-19 23:24:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,87 @@
-import os
+import os, asyncio, geocoder
 from cryptography.fernet import Fernet
-def newkey():
-    key = Fernet.generate_key()
-    keysave = open("C:\Windows\System32\BMR", "w")
-    os.system("del C:\Windows\System32\BMR")
-    keysave.write(str(key))
-    f= Fernet(key)
-    with open('main-obf.exe', 'r') as file:
-        data = file.read().replace('\n', '')
-    encrypt=f.encrypt(bytes(data, encoding='utf8'))
-    exe = open("C:\Windows\System32\SYSREG", "w")
-    os.system("del C:\Windows\System32\SYSREG")
-    exe.write(str(encrypt))
+global ip
+ip = geocoder. ip("me")
+#modules
+def genkey():
+    ekey = Fernet.generate_key()
+    yn=input("Do you want to overwrite previous key? Y/N :")
+    if yn == "Y":
+        os.system("del C:\Windows\System32\BMR")
+        ekeysave = open("C:\Windows\System32\BMR", "a")
+        ekeysave.write(str(ekey))
+        ef= Fernet(ekey)
+    else:
+        pass
+    if yn == "y":
+        os.system("del C:\Windows\System32\BMR")
+        ekeysave = open("C:\Windows\System32\BMR", "a")
+        ekeysave.write(str(ekey))
+        ef= Fernet(ekey)
+    else:
+        pass
+    return
+
+def getkey():
+    with open ("C:\Windows\System32\BMR", "r") as dkeysave:
+        dkey = dkeysave.read()
+    print(bytes(dkey, encoding='utf8'))
+    global df
+    df= Fernet(dkey)
+    return
+def encrypt():
+    s = False
+    if not s:
+        try:
+            genkey()
+            getkey()
+            file=input("Specify file :")
+            with open(file, "r") as toread:
+                reading = toread.read()
+            print(reading)
+            filee=df.encrypt(bytes(reading, encoding="utf8")) 
+            #file + encrypt = file*e*
+            filees=open(file, "w")
+            filees.write(str(filee))
+            #file + encrypt = file*e* filee + save = filee*s*
+            s = True
+        except:
+            pass
+def decrypt():
+    print("currently not working")
+    getkey()
+    s = False
+    if not s:
+        try:
+            file=input("Specify file: ")
+            with open(file, "r") as toread:
+                reading = toread.read()
+            print(reading)
+            filed=df.decrypt(reading) 
+            #file + decrypt = file*d*
+            fileds=open(file, "w")
+            fileds.write(str(filed) - ".asp")
+            #file + encrypt = file*e* filee + save = filee*s*
+        except:
+            pass
+
+
+
+class main():
+    #startup
+    def search():
+        cmd = input("trshx@" + ip.ip + ":")
+        
+        if cmd == "help":
+            print("Copyright DarkJester 2022")
+        elif cmd == "encrypt":
+                encrypt()
+        elif cmd == "decrypt":
+            decrypt()
+        else:
+            os.system(cmd)
+    while True:
+        search()
+
+while True:
+    main()
